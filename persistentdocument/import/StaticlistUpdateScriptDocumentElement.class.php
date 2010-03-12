b8423f1cdea524d422498c5a45694d406152925a
Gaël PORT <gael.port@rbs.fr>
2010-03-12 13:47:14 +0000

    [NEW] Added new staticlistupdate element to add new items in a static list without overwriting existing ones.

---
+++ persistentdocument/import/StaticlistUpdateScriptDocumentElement.class.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * list_StaticlistUpdateScriptDocumentElement
+ * @package modules.list.persistentdocument.import
+ */
+class list_StaticlistUpdateScriptDocumentElement extends import_ScriptDocumentElement
+{
+	/**
+	 * @return f_persistentdocument_PersistentDocument
+	 */
+	protected function initPersistentDocument()
+	{
+		$listid = $this->attributes['listid'];
+		return list_ListService::getInstance()->getDocumentInstanceByListId($listid);
+	}
+	
+	protected function saveDocument()
+	{
+	}
+	
+	public function endProcess()
+	{
+		$list = $this->getPersistentDocument();
+		$children = $this->script->getChildren($this);
+		
+		$items = $list->getItems();
+		if (count($children))
+		{
+			foreach ($children as $scriptElement)
+			{
+				if ($scriptElement instanceof list_StaticitemElement)
+				{
+					$newItem = $scriptElement->getStaticItem();
+					$value = $newItem->getValue();
+					$add = true;
+					foreach ($items as $item)
+					{
+						if ($item->getValue() == $value)
+						{
+							if ($item->getLabel() == $newItem->getLabel())
+							{
+								$add = false;
+								break;
+							}
+							throw new Exception('Item with value '.$value.' already exists with an other label!');
+						}
+					}					
+					if ($add)
+					{
+						$items[] = $scriptElement->getStaticItem();
+					}
+				}
+			}
+		}
+		
+		$list->setItemvalues(serialize($items));
+		if ($list->isModified())
+		{
+			$list->save();
+		}
+	}
+	
+	/**
+	 * @return f_persistentdocument_PersistentDocumentModel
+	 */
+	protected function getDocumentModel()
+	{
+		return f_persistentdocument_PersistentDocumentModel::getInstanceFromDocumentModelName('modules_list/staticlist');
+	}
+}
--- persistentdocument/staticlist.class.php
+++ persistentdocument/staticlist.class.php
@@ -1,10 +1,22 @@
 <?php
 class list_persistentdocument_staticlist extends list_persistentdocument_staticlistbase
 {
+	/**
+	 * @var String
+	 */
 	private $deserializedValues;
+	
+	/**
+	 * @param String $value the serialized array of items.
+	 */
+	public function setItemvalues($value)
+	{
+		$this->deserializedValues = null;
+		parent::setItemvalues($value);
+	}
+	
 	/**
 	 * Return the list of items. Use the property itemvalues to extract of the serialized string the list of items
-	 *
 	 * @return array of list_Item
 	 */
 	public function getItems()
@@ -24,5 +36,4 @@ class list_persistentdocument_staticlist extends list_persistentdocument_staticl
 		}
 		return $this->deserializedValues;
 	}
-	
 }
