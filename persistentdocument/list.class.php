08f82d5ba91e4a5c78a3cb5e5275f2294ac441a9
Franck STAUFFER <franck.stauffer@rbs.fr>
2010-07-01 22:30:45 +0200

    [FIX] Translated label for lists in backoffice

--- persistentdocument/list.class.php
+++ persistentdocument/list.class.php
@@ -5,6 +5,36 @@
  */
 class list_persistentdocument_list extends list_persistentdocument_listbase
 {
+	
+	/**
+	 * @see f_persistentdocument_PersistentDocumentImpl::getLabel()
+	 *
+	 * @return String
+	 */
+	public function getLabel()
+	{
+		$label = parent::getLabel();
+		if (f_Locale::isLocaleKey($label))
+		{
+			return f_Locale::translateUI($label);
+		}
+		return $label;
+	}
+	
+	/**
+	 * @see list_persistentdocument_listbase::getDescription()
+	 *
+	 * @return String
+	 */
+	public function getDescription()
+	{
+		$description = parent::getDescription();
+		if (f_Locale::isLocaleKey($description))
+		{
+			return f_Locale::translateUI($description);
+		}
+		return $description;
+	}
     /**
      * @return list_Item[]
      */
