3f4e8f5992174e1baa4261eb87e1c122fa7c71ff
Gaël PORT <gael.port@rbs.fr>
2011-07-27 16:06:04 +0200

    [REFACTOR] Replace usage of deprecated classes and methods. [DELETE] Delete some deprecated methods.

--- persistentdocument/import/EditableitemScriptDocumentElement.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * list_EditableitemScriptDocumentElement
- * @package modules.list.persistentdocument.import
- * @deprecated use list_ItemScriptDocumentElement
- */
-class list_EditableitemScriptDocumentElement extends list_ItemScriptDocumentElement
-{
-}
