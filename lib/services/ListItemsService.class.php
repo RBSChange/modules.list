76d8bf6722bf8614d0935d03ada2ecef2757d4be
Loïc Couturier <loic.couturier@rbs.fr>
2012-08-02 15:59:19 +0200

    [UPDATE] Clean bad comment

--- lib/services/ListItemsService.class.php
+++ lib/services/ListItemsService.class.php
@@ -1,24 +1,8 @@
 <?php
-/**
- * @date Tue Mar 06 15:31:43 CET 2007
- * @author inthrycn
- */
 interface list_ListItemsService
 {
 	/**
 	 * @return list_Item[]
 	 */
 	public function getItems();
-	
-	/**
-	 * Optional.
-	 * @param String $value
-	 */
-//	public function getItemByValue($value);
-	
-	/**
-	 * Optional.
-	 * @param Array $paramters
-	 */
-//	public function setParameters($paramters);
 }
